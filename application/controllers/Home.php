cce65316f96b1ce3401336fc3795ab34b3af337a
Gun Gun Priatna <gungunpriatna02@gmail.com>
2021-10-01 20:34:13 +0700

    refactor function pendaftaran()

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -48,6 +48,79 @@ class Home extends CI_Controller {
 	
 	public function pendaftaran() 
 	{
+		$this->set_registration_validation();
+		
+		if ($this->form_validation->run() === FALSE) 
+		{
+			$data = [
+				'title' => 'Pendaftaran PDPT - Pangkalan Data Pendidikan Tinggi',
+				'isi'  => 'home/form_pendaftaran'
+			];
+
+			$this->load->view('layout/wrapper',$data);	
+		}
+		else
+		{	
+						
+			$registration_data = $this->set_registration_data();
+						
+			$this->pdpt_model->add_mahasiswa($registration_data['mahasiswa']);
+			$this->pdpt_model->add_user($registration_data['user']);
+			$this->pdpt_model->hak_akses($registration_data['access']);
+			
+			$data = [
+				'title'		=> 'Pendaftaran Berhasil - Pangkalan Data Pendidikan Tinggi',
+				'isi'  		=> 'home/pendaftaran_berhasil'
+			];
+						
+			$this->load->view('layout/wrapper',$data);
+		}
+	}
+
+	private function set_registration_data()
+	{
+		$npm = $this->input->post('npm');
+		$email = $this->input->post('email');
+		$password = $this->input->post('password');
+
+		$registration_data = [
+			'mahasiswa' => [
+				'npm' 	=> $npm,
+				'nama_mahasiswa'	=> $this->input->post('nama_mahasiswa'),
+				'tempat_lahir' 		=> $this->input->post('tempat_lahir'),
+				'tanggal_lahir'	=> $this->input->post('tanggal_lahir'),
+				'jenis_kelamin'	=> $this->input->post('jenis_kelamin'),
+				'agama'	=> $this->input->post('agama'),
+				'nomor_ktp'	=> $this->input->post('nomor_ktp'),
+				'alamat_rumah'	=> $this->input->post('alamat_rumah'),
+				'alamat_desa'	=> $this->input->post('alamat_desa'),
+				'alamat_kelurahan'	=> $this->input->post('alamat_kelurahan'),
+				'alamat_kecamatan'	=> $this->input->post('alamat_kecamatan'),
+				'alamat_kabupaten'	=> $this->input->post('alamat_kabupaten'),
+				'kode_pos'	=> $this->input->post('kode_pos'),
+				'telepon_rumah'	=> $this->input->post('telepon_rumah'),
+				'telepon_genggam'	=> $this->input->post('telepon_genggam'),
+				'email'	=> $email,
+				'nama_penerima_kps'	=> $this->input->post('nama_penerima_kps'),
+				'nomor_penerima_kps'	=> $this->input->post('nomor_penerima_kps'),
+				'tipe_tempat_tinggal'	=> $this->input->post('tipe_tempat_tinggal')
+			],
+			'user' => [
+				'username' 	=> $npm,
+				'userpassword'	=> password_hash($password, PASSWORD_BCRYPT),
+				'useremail'	=> $email
+			],
+			'access' => [
+				'username' 	=> $npm,
+				'hak_akses'	=> 'user'
+			]
+			];
+
+		return $registration_data;
+	}
+	
+	private function set_registration_validation()
+	{
 		$this->form_validation->set_rules('npm', 'NPM', 'required|numeric|max_length[8]');
 		$this->form_validation->set_rules('nama_mahasiswa', 'Nama Mahasiswa', 'required');
 		$this->form_validation->set_rules('tempat_lahir', 'Tempat Lahir', 'required');
@@ -66,64 +139,8 @@ class Home extends CI_Controller {
 		$this->form_validation->set_rules('email', 'Email', 'required|valid_email');
 		$this->form_validation->set_rules('password', 'Password', 'required');
 		$this->form_validation->set_rules('passconf', 'Password Confirmation', 'required|matches[password]');
-		
-		if ($this->form_validation->run() === FALSE) 
-		{
-			$data=array('title'=>'Pendaftaran PDPT - Pangkalan Data Pendidikan Tinggi',
-						'isi'  =>'home/form_pendaftaran'					
-							);
-			$this->load->view('layout/wrapper',$data);	
-		}
-		else
-		{	
-			$npm = $this->input->post('npm');
-			$email = $this->input->post('email');
-			
-			$data_mahasiswa = array(
-						'npm' 	=> $npm,
-						'nama_mahasiswa'	=> $this->input->post('nama_mahasiswa'),
-						'tempat_lahir' 		=> $this->input->post('tempat_lahir'),
-						'tanggal_lahir'	=> $this->input->post('tanggal_lahir'),
-						'jenis_kelamin'	=> $this->input->post('jenis_kelamin'),
-						'agama'	=> $this->input->post('agama'),
-						'nomor_ktp'	=> $this->input->post('nomor_ktp'),
-						'alamat_rumah'	=> $this->input->post('alamat_rumah'),
-						'alamat_desa'	=> $this->input->post('alamat_desa'),
-						'alamat_kelurahan'	=> $this->input->post('alamat_kelurahan'),
-						'alamat_kecamatan'	=> $this->input->post('alamat_kecamatan'),
-						'alamat_kabupaten'	=> $this->input->post('alamat_kabupaten'),
-						'kode_pos'	=> $this->input->post('kode_pos'),
-						'telepon_rumah'	=> $this->input->post('telepon_rumah'),
-						'telepon_genggam'	=> $this->input->post('telepon_genggam'),
-						'email'	=> $email,
-						'nama_penerima_kps'	=> $this->input->post('nama_penerima_kps'),
-						'nomor_penerima_kps'	=> $this->input->post('nomor_penerima_kps'),
-						'tipe_tempat_tinggal'	=> $this->input->post('tipe_tempat_tinggal')
-					);
-			$data_user = array(
-						'username' 	=> $npm,
-						'userpassword'	=> md5($this->input->post('password')),
-						'useremail'	=> $email
-					);
-					
-			$data_akses = array(
-						'username' 	=> $npm,
-						'hak_akses'	=> 'user'
-					);
-				
-					
-			$this->pdpt_model->add_mahasiswa($data_mahasiswa);
-			$this->pdpt_model->add_user($data_user);
-			$this->pdpt_model->hak_akses($data_akses);
-			
-			$data=array('title'		=>'Pendaftaran Berhasil - Pangkalan Data Pendidikan Tinggi',
-						'isi'  		=>'home/pendaftaran_berhasil'
-						);
-						
-			$this->load->view('layout/wrapper',$data);
-		}
 	}
-	
+
 	public function pencarian() 
 	{
 		$data=array('title'		=>'Pencarian Data',
